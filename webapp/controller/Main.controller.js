310b7b5b8582da60d11644c03a66fc0db1a5d17c
Maximilian Olzinger <mo@clouddna.at>
2022-10-26 12:39:26 +0000

    Exercise 7.5

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
+        "sap/m/MessageBox"
     ],
-    function(BaseController) {
+    function(BaseController, MessageBox) {
       "use strict";
   
       return BaseController.extend("at.clouddna.training00.zhoui5.controller.Main", {
@@ -20,6 +21,33 @@ sap.ui.define(
 
         onEmailPress: function(event){
             sap.m.URLHelper.triggerEmail(event.getSource().getText());
+        },
+
+        onCustomerDelete: function(event){
+            let path = event.getParameter("listItem").getBindingContext().getPath(),
+                model = this.getView().getModel(),
+                resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            MessageBox.warning(resourceBundle.getText("msg.confirm.delete.text"), {
+               title: resourceBundle.getText("msg.confirm.delete.title"),
+               actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+               emphasizedAction: MessageBox.Action.YES,
+               onClose: (chosenAction)=>{
+                   if(chosenAction === MessageBox.Action.YES){
+                       model.remove(path, {
+                           success: (data, respons)=>{
+                                MessageBox.success(resourceBundle.getText("msg.delete.success"));
+                           },
+                           error: (error)=>{
+                                MessageBox.error(error.message);
+                           }
+                       });
+                   }
+               }
+            });
+
+
+            debugger;
         }
       });
     }
