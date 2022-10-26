6d414d4bd44b4f85e7d551fe71c59174d49dd1cf
Maximilian Olzinger <mo@clouddna.at>
2022-10-26 11:27:57 +0000

    Exercise 7.2

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -13,7 +13,7 @@ sap.ui.define(
             const view = this.getView(),
                 i18nModel = view.getModel("i18n"),
                 resourceBundle = i18nModel.getResourceBundle(),
-                formattedText = resourceBundle.getText(`gender.${genderKey}`);
+                formattedText = resourceBundle.getText(`gender.${genderKey == 0 ? 'male' : 'female'}`);
 
             return formattedText;
         },
