875da69c5ab591d933bb9873a2e230476417f680
Maximilian Olzinger <mo@clouddna.at>
2022-10-26 09:20:37 +0000

    Exercise 6.1

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("at.clouddna.training00.zhoui5.controller.Main", {
+        onInit() {
+        },
+
+        genderFormatter: function(genderKey){
+            const view = this.getView(),
+                i18nModel = view.getModel("i18n"),
+                resourceBundle = i18nModel.getResourceBundle(),
+                formattedText = resourceBundle.getText(`gender.${genderKey}`);
+
+            return formattedText;
+        },
+
+        onEmailPress: function(event){
+            sap.m.URLHelper.triggerEmail(event.getSource().getText());
+        }
+      });
+    }
+  );
+  
